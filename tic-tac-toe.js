aa17ddbf0b0548d86d0a6e687a7852cdc03b487a
remzr <110097561+remzr@users.noreply.github.com>
2025-10-26 22:51:26 +0100

    Setup simple gameloop for single player with win-conditions

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,54 @@
-console.log("JS-loaded");
+// Gameboard Object
+/*function Gameboard() {
+    const fields = 9;
+    const board = [];
+
+/*    //Create cells based on field value (9)
+    for (let i=0; i < fields; i++) {
+        board[i].push(Cell(i));
+    }; 
+    return fields;
+};*/
+
+/* Cell Object
+function Cell(number) {
+    let value = number;
+    return value;
+};*/    
+
+
+// Gameflow Object
+function gameLoop() {
+//    const fieldAmount = Gameboard();
+    
+    let userInput = 0;
+    let turn = 0;
+
+        for (let i = 1; i < 9; i++) {
+        if (userInput == 123||
+            userInput == 456||
+            userInput == 789||
+            userInput == 147||
+            userInput == 258||
+            userInput == 369||
+            userInput == 159||
+            userInput == 753){
+            console.log("User wins:" + userInput);
+        } else if (turn >= 9) {
+            console.log("Draw");
+        } else if (turn < 9) {
+            let userNumber = prompt(`Enter a number ${turn}`);
+            userInput = userInput + userNumber;
+            turn++;
+            console.log(turn + userNumber);
+        } else {
+            console.log("Error");
+        }
+    }
+}
+
+gameLoop();
+
+
+
+
