5889ac1563f2a4bae824f67e51cece884055f0ba
remzr <110097561+remzr@users.noreply.github.com>
2025-10-27 21:59:24 +0100

    Start to redo gameloop. Outline for now, not fully working

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -20,7 +20,7 @@ function playerCreator(name, token, selection) {
     return {
         name: name,
         token: token,
-        selection: selection,
+        selection: selection + prompt(`${name} picks a Number:`),
         winner() {
             winningNumbers =  ["123", "456","789","147","258","369","159","753"];
             sortedSelection = selection.split("").sort().join("");
@@ -28,7 +28,7 @@ function playerCreator(name, token, selection) {
 
             for (let i = 0; i < winningNumbers.length; i++) {
                 if (sortedSelection.includes(winningNumbers[i]) === true) {
-                    return(`${this.name} wins!`);
+                    return(`${name} wins!`);
                 }
             }
         }
@@ -39,39 +39,30 @@ function playerCreator(name, token, selection) {
 function gameLoop() {
 
     //Create players with creator
-    let player1 = playerCreator("playerOne", "X", "6217");
-    let player2 = playerCreator("playerTwo", "O", 459);
-    
-
-    console.log(player1.winner());
-
-    let userInput = 123;
-    
+    let playerOne = playerCreator("playerOne", "X");
+    let playerTwo = playerCreator("playerTwo", "O");
+       
     // Create turn variable to count turns
     let turn = 1;
 
-        for (let i = 1; i < 9; i++) {
-        if (userInput == 123||
-            userInput == 456||
-            userInput == 789||
-            userInput == 147||
-            userInput == 258||
-            userInput == 369||
-            userInput == 159||
-            userInput == 753){
-//            console.log("User wins:" + userInput);
-        } else if (turn >= 9) {
-            console.log("Draw");
-        } else if (turn < 9) {
-            let userNumber = prompt(`Enter a number ${turn}`);
-            userInput = userInput + userNumber;
-            turn++;
-            console.log(turn + userNumber);
+    //Gameloop
+    do {
+        if (playerOne.winner != undefined) {
+            console.log(playerOne.winner);
+        } else if (playerCreator.winner != undefined) {
+            console.log(playerTwo.winner);
+        } else if (turn >=9) {
+            console.log("Its a draw!");
+        } else if (turn % 2 != 0) {
+            playerOne.selection;
         } else {
-            console.log("Error");
+            playerTwo.selection;
         }
-    }
-}
+        turn++;
+        }
+    while (turn > 9);
+
+} 
 
 gameLoop();
 
